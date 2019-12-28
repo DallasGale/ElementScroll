9f0917acf3dc6ba37d2d72a4449aac598878c2c2
Dallas Gale <dallasgale.digital@gmail.com>
2019-12-28 12:00:32 +1100

    console logs removed

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const percentageScrolled = document.getElementById("percentage-scrolled");
 const scroller = document.getElementById("scroller");
 
 let windowHeight = document.documentElement.clientHeight;
-console.log("windowHeight", windowHeight);
-console.log("referenceElement", referenceElement.getBoundingClientRect());
+// console.log("windowHeight", windowHeight);
+// console.log("referenceElement", referenceElement.getBoundingClientRect());
 
 if (window) {
   window.addEventListener("resize", () => {
     windowHeight = document.documentElement.clientHeight;
-    console.log("windowHeight", windowHeight);
+    // console.log("windowHeight", windowHeight);
   });
   window.removeEventListener("resize", () => {
     console.log("resize listener removed");
@@ -25,11 +25,11 @@ if (window) {
       window.addEventListener("scroll", () => {
         const currentValue = referenceElement.getBoundingClientRect().y;
         const formattedCurrentValue = Math.abs(currentValue).toFixed(2);
-        console.log("formattedCurrentValue", formattedCurrentValue);
+        // console.log("formattedCurrentValue", formattedCurrentValue);
         const number = FormatToPercent(
           ConvertNumberToPercentage(formattedCurrentValue, 5000)
         );
-        const output = Math.abs(number).toFixed(4);
+        const output = Math.abs(number).toFixed(2);
         scroller.style.top = output + "%";
         percentageScrolled.innerText = output;
       });
